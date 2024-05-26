4b60ed384fe927111e0a24cc76c2e35fb4df28e1
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-26 15:34:15 +0900

    [Setting]: 프로젝트 생성

---
+++ src/main/java/org/haedal/zzansuni/ZzansuniApplication.java
@@ -0,0 +1,13 @@
+package org.haedal.zzansuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzansuniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzansuniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/haedal/zzansuni/ZzansuniApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haedal.zzansuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzansuniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
