0b5ea3cac2c1989c341d6d20c15cf44a110b0627
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-29 11:49:16 +0900

    [Feat]: 유저 인프라 레이어

---
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/infrastructure/user/UserRepository.java
@@ -0,0 +1,10 @@
+package org.haedal.zzansuni.infrastructure.user;
+
+import org.haedal.zzansuni.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByAuthToken(String authToken);
+}
