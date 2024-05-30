db86ea3a64a9c5e73843e5c5fd0c6f6aece66906
momnpa333 <momnpa333@gmail.com>
2024-05-31 04:01:52 +0900

    [Feat]: ChallengeReader 제작
    #5

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/challenge/ChallengeReader.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/challenge/ChallengeReader.java
@@ -1,5 +1,10 @@
 package org.haedal.zzansuni.domain.challengegroup.challenge;
 
+import java.util.Optional;
+
 public interface ChallengeReader {
 
+    Challenge getById(Long id);
+
+    Optional<Challenge> findById(Long id);
 }
--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/infrastructure/challengegroup/challenge/ChallengeReaderImpl.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/infrastructure/challengegroup/challenge/ChallengeReaderImpl.java
@@ -1,7 +1,12 @@
 package org.haedal.zzansuni.infrastructure.challengegroup.challenge;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.haedal.zzansuni.domain.challengegroup.challenge.Challenge;
+import org.haedal.zzansuni.domain.challengegroup.challenge.ChallengeModel;
 import org.haedal.zzansuni.domain.challengegroup.challenge.ChallengeReader;
+import org.haedal.zzansuni.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +14,17 @@ import org.springframework.stereotype.Component;
 public class ChallengeReaderImpl implements ChallengeReader {
 
     private final ChallengeRepository challengeRepository;
+
+    @Override
+    public Challenge getById(Long id) {
+        return findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("Challenge", id));
+    }
+
+    @Override
+    public Optional<Challenge> findById(Long id) {
+        return challengeRepository.findById(id);
+    }
 
 
 }
