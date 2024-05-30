3b8b4d9ff13278e3842c709eafe12e9ef68eb342
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-30 19:45:17 +0900

    [Feat]: 회원가입 Controller Layer 구현 (#4)

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/controller/auth/AuthController.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/controller/auth/AuthController.java
@@ -6,6 +6,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.haedal.zzansuni.controller.user.UserRes;
 import org.haedal.zzansuni.core.api.ApiResponse;
+import org.haedal.zzansuni.domain.auth.AuthService;
+import org.haedal.zzansuni.domain.user.UserModel;
+import org.haedal.zzansuni.global.jwt.JwtToken;
+import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
+    private final AuthService authService;
 
     @Operation(summary = "oauth2 로그인", description = "oauth2 code를 이용하여 로그인한다.")
     @PostMapping("/api/auth/oauth2")
     public ApiResponse<AuthRes.LoginResponse> oauth2(@RequestBody @Valid AuthReq.OAuth2LoginRequest request) {
-        return ApiResponse.success(
-                new AuthRes.LoginResponse("accessToken", "refresh", new UserRes.UserInfoDto(
-                        1L, "nickname", "https://picsum.photos/200/300", "email",
-                        new UserRes.TierInfoDto("tier", 100, 50)
-                )));
+        Pair<JwtToken,UserModel> pair = authService.oAuth2LoginOrSignup(request.provider(), request.code(), request.state());
+        var response = AuthRes.LoginResponse.from(pair.getFirst(), pair.getSecond());
+        return ApiResponse.success(response);
     }
 
     @Operation(summary = "로그아웃", description = "로그아웃한다.")
--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/controller/auth/AuthReq.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/controller/auth/AuthReq.java
@@ -1,14 +1,16 @@
 package org.haedal.zzansuni.controller.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.haedal.zzansuni.domain.auth.OAuth2Provider;
 
 public class AuthReq {
     public record OAuth2LoginRequest(
-            @NotBlank(message = "provider는 필수입니다.")
+            @NotNull(message = "provider는 필수입니다.")
             OAuth2Provider provider,
             @NotBlank(message = "code는 필수입니다.")
-            String code
+            String code,
+            String state
     ) {
     }
 }
