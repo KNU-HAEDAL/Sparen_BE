445d9298a30601e175dc0eb2c15563c9207d8040
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-31 17:04:41 +0900

    [Feat]: 유저챌린지 INSERT 추가 (#7)

---
+++ utils/node/make.user.challenge.js
@@ -0,0 +1,13 @@
+import { fakerKO as faker } from "@faker-js/faker";
+
+for (let i = 0; i < 50; i++) {
+    const challengeId = faker.number.int({ min: 1, max: 40 });
+    const userId = faker.number.int({ min: 1, max: 20 });
+    const createdAt = faker.date.past().toISOString().slice(0, 19).replace('T', ' ');
+    const lastModifiedAt = faker.date.recent().toISOString().slice(0, 19).replace('T', ' ');
+    const statuses = ['FINISHED', 'PROCEEDING'];
+    const status = statuses[Math.floor(Math.random() * statuses.length)];
+
+    const sql = `(${challengeId}, '${createdAt}', '${lastModifiedAt}', ${userId}, '${status}'),`;
+    console.log(sql);
+}
