5170230d6790d83359e39f8e680eaa9b4aebff58
momnpa333 <momnpa333@gmail.com>
2024-05-31 15:02:24 +0900

    [Feat]: ChallengeService에 Verification 저장로직 제작
    #5

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/challenge/ChallengeService.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/challenge/ChallengeService.java
@@ -1,9 +1,11 @@
 package org.haedal.zzansuni.domain.challengegroup.challenge;
 
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.haedal.zzansuni.domain.user.UserReader;
-import org.haedal.zzansuni.domain.user.UserStore;
+import org.haedal.zzansuni.domain.challengegroup.challenge.ChallengeCommand.Verificate;
+import org.haedal.zzansuni.domain.challengegroup.challengeverification.ChallengeVerification;
+import org.haedal.zzansuni.domain.challengegroup.challengeverification.ChallengeVerificationStore;
+import org.haedal.zzansuni.domain.challengegroup.userchallenge.UserChallenge;
+import org.haedal.zzansuni.domain.challengegroup.userchallenge.UserChallengeReader;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -13,5 +15,17 @@ public class ChallengeService {
     private final ChallengeReader challengeReader;
     private final ChallengeStore challengeStore;
 
-    
+    private final UserChallengeReader userChallengeReader;
+
+    private final ChallengeVerificationStore challengeVerificationStore;
+
+    /**
+     * 챌린지 인증하기
+     */
+    public void verification(Long userChallengeId, Verificate verificate) {
+        UserChallenge userChallenge = userChallengeReader.getById(userChallengeId);
+        ChallengeVerification challengeVerification = ChallengeVerification.create(verificate,
+            userChallenge);
+        challengeVerificationStore.store(challengeVerification);
+    }
 }
