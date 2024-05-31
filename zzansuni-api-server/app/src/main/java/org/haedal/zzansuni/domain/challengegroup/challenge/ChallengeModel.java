3f3433c3a227f618ca87d372ca39a53d7483ba88
momnpa333 <momnpa333@gmail.com>
2024-05-31 21:42:15 +0900

    [Feat]: ChallengeModel에 ChallengeRecordDto 추가
    #5

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/challenge/ChallengeModel.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/challenge/ChallengeModel.java
@@ -1,10 +1,15 @@
 package org.haedal.zzansuni.domain.challengegroup.challenge;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.haedal.zzansuni.domain.challengegroup.ChallengeGroup;
 import org.haedal.zzansuni.domain.challengegroup.DayType;
+import org.haedal.zzansuni.domain.challengegroup.challengeverification.ChallengeVerification;
+import org.haedal.zzansuni.domain.challengegroup.userchallenge.UserChallenge;
 
 @Getter
 @Builder
@@ -35,9 +40,9 @@ public class ChallengeModel {
     @AllArgsConstructor
     public static class ChallengeVerificationResult {
 
-        private Integer totalCount;
-        private Integer successCount;
-        private Integer obtainExp;
+        private final Integer totalCount;
+        private final Integer successCount;
+        private final Integer obtainExp;
 
         public static ChallengeVerificationResult from(Integer totalCount, Integer successCount,
             Integer obtainExp) {
@@ -45,6 +50,33 @@ public class ChallengeModel {
                 .totalCount(totalCount)
                 .successCount(successCount)
                 .obtainExp(obtainExp)
+                .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class ChallengeRecordDto {
+
+        private final String title;
+        private final Integer totalCount;
+        private final Integer successCount;
+        private final LocalDate startDate;
+        private final LocalDate endDate;
+        private final List<Long> recordIds;
+
+        public static ChallengeRecordDto from(Challenge challenge, ChallengeGroup challengeGroup,
+            List<ChallengeVerification> challengeVerificationList) {
+            return ChallengeRecordDto.builder()
+                .title(challengeGroup.getTitle())
+                .totalCount(challenge.getRequiredCount())
+                .successCount(challengeVerificationList.size())
+                .startDate(challenge.getStartDate())
+                .endDate(challenge.getEndDate())
+                .recordIds(challengeVerificationList.stream()
+                    .map(ChallengeVerification::getId)
+                    .collect(Collectors.toList()))
                 .build();
         }
     }
