fb27b0164269222be8e38dd3f0473449be5d6062
momnpa333 <momnpa333@gmail.com>
2024-06-01 03:06:05 +0900

    [Feat]: 리뷰 관련 req tocommand 제작
    #5

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/controller/challengegroup/challenge/ChallengeReq.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/controller/challengegroup/challenge/ChallengeReq.java
@@ -1,15 +1,27 @@
 package org.haedal.zzansuni.controller.challengegroup.challenge;
 
+import org.haedal.zzansuni.domain.challengegroup.challenge.ChallengeCommand;
+import org.haedal.zzansuni.domain.challengegroup.challenge.ChallengeModel;
+
 public class ChallengeReq {
 
     public record ChallengeVerificationRequest(
-            String content
+        String content
     ) {
+
     }
 
     public record ChallengeReviewCreateRequest(
-            String content,
-            Integer rating
+        String content,
+        Integer rating
     ) {
+
+        public ChallengeCommand.ReviewCreate toCommand() {
+            return ChallengeCommand.ReviewCreate.builder()
+                .content(content)
+                .rating(rating)
+                .build();
+        }
     }
+
 }
