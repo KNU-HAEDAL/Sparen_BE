02ca8119bad4fc09c8d2e322a0e0ceebb56025c9
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-31 15:38:08 +0900

    [Feat]: temp

---
+++ zzansuni-api-server/app/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (exp, created_at, last_modified_at, auth_token, email, nickname, password, profile_image_url, provider, role)
+VALUES (0, '2023-05-31 10:00:00', '2023-05-31 12:00:00', 'NAVER_HP6Yyh7SIkk8QS1VFtEylxrxo23UxNTBHcAjfTw-BHM', 'null', 'bayy', 'null', 'null', 'NAVER', 'ADMIN');
