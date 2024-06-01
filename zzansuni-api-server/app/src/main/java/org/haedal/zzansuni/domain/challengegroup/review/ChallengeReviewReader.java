a63b21565d61704a32ed0d0deaa375689d8393df
momnpa333 <momnpa333@gmail.com>
2024-06-02 00:01:14 +0900

    [Feat]: ReviewReader에 리뷰 페이징 추가
    #25

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/review/ChallengeReviewReader.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/domain/challengegroup/review/ChallengeReviewReader.java
@@ -3,6 +3,8 @@ package org.haedal.zzansuni.domain.challengegroup.review;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ChallengeReviewReader {
 
@@ -11,4 +13,8 @@ public interface ChallengeReviewReader {
     Optional<ChallengeReview> findByUserChallengeId(Long challengeId);
 
     Map<Long, Boolean> getReviewWrittenMapByUserChallengeId(List<Long> userChallengeIds);
+
+    Page<ChallengeReview> getChallengeReviewPageByChallengeGroupId(Long challengeGroupId,
+        Pageable pageable);
+
 }
