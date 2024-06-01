e9f3fb053833f9eac86be9e467a73bcff3b4cf43
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-06-01 15:28:18 +0900

    [Fix]: bug fix

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/infrastructure/challengegroup/challenge/ChallengeRepository.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/infrastructure/challengegroup/challenge/ChallengeRepository.java
@@ -11,24 +11,5 @@ import org.springframework.data.repository.query.Param;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
-    @Query(
-        "SELECT new org.haedal.zzansuni.domain.challengegroup.challenge.ChallengeCurrentDto("
-            +
-            "c.id, cg.title, " +
-            "(SELECT COUNT(cv) FROM ChallengeVerification cv WHERE cv.userChallenge.challenge.id = c.id AND cv.userChallenge.user.id = :userId), "
-            +
-            "c.requiredCount, " +
-            "uc.createdAt, c.startDate, c.endDate, " +
-            "cg.category, " +
-            "(SELECT COUNT(cr) > 0 FROM ChallengeReview cr WHERE cr.userChallenge.challenge.id = c.id AND cr.userChallenge.user.id = :userId)"
-            +
-            ") " +
-            "FROM Challenge c " +
-            "JOIN c.challengeGroup cg " +
-            "JOIN UserChallenge uc ON uc.challenge.id = c.id " +
-            "WHERE uc.user.id = :userId " +
-            "AND uc.status = org.haedal.zzansuni.domain.challengegroup.challenge.ChallengeStatus.PROCEEDING ")
-    Page<ChallengeCurrentDto> findAllCurrentChallenges(@Param("userId") Long userId,
-        Pageable pageable);
 }
 
