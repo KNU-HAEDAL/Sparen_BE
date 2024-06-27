b4777be9923c7ad1e9c099339c1096285a377e71
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-06-27 20:08:21 +0900

    [Feat]: 챌린지 그룹 생성,삭제 테스트코드 작성

---
+++ zzansuni-api-server/app/src/test/java/org/haedal/zzansuni/domain/challengegroup/ChallengeGroupQueryServiceTest.java
@@ -0,0 +1,28 @@
+package org.haedal.zzansuni.domain.challengegroup;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChallengeGroupQueryServiceTest {
+
+    @Test
+    void getChallengeGroupsPaging() {
+    }
+
+    @Test
+    void getChallengeGroupDetail() {
+    }
+
+    @Test
+    void getChallengeGroupsShortsPaging() {
+    }
+
+    @Test
+    void getChallengeGroupRankingsPaging() {
+    }
+
+    @Test
+    void getChallengeGroupRanking() {
+    }
+}
