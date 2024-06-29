0ac7f6fcb278f71590c00ddd277bcacd6e840db9
momnpa333 <momnpa333@gmail.com>
2024-06-29 18:12:48 +0900

    [Test]: UserChallengeServiceTest, ChallengeServiceTest 스켈레톤 코드 제작

---
+++ zzansuni-api-server/app/src/test/java/org/haedal/zzansuni/domain/challengegroup/challenge/ChallengeServiceTest.java
@@ -0,0 +1,35 @@
+package org.haedal.zzansuni.domain.challengegroup.challenge;
+
+import org.junit.jupiter.api.Test;
+
+public class ChallengeServiceTest {
+
+    @Test
+    void getChallengeRecord() {
+    }
+
+    @Test
+    void getChallengeRecordDetail() {
+
+    }
+
+    @Test
+    void createReview() {
+
+    }
+
+    @Test
+    void getChallengeReviewsByGroupId() {
+
+    }
+
+    @Test
+    void getChallengeReviews() {
+
+    }
+
+    @Test
+    void getChallengeGroupReviewScore() {
+
+    }
+}
---
+++ zzansuni-api-server/app/src/test/java/org/haedal/zzansuni/domain/challengegroup/userchallenge/UserChallengeServiceTest.java
@@ -0,0 +1,26 @@
+package org.haedal.zzansuni.domain.challengegroup.userchallenge;
+
+import org.junit.jupiter.api.Test;
+
+public class UserChallengeServiceTest {
+
+    @Test
+    void participateChallenge() {
+    }
+
+    @Test
+    void verification() {
+
+    }
+
+    @Test
+    void getCurrentChallenges() {
+
+    }
+
+    @Test
+    void getCompletedChallenges() {
+
+    }
+
+}
