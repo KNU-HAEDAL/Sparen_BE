31e60da2892c765486c61b9a765e74094d3c3f5e
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-07-17 23:50:22 +0900

    [Revert] Swagger server url

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/global/api/SwaggerConfig.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/global/api/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package org.haedal.zzansuni.global.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +10,12 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://api.reditus.site",description = "Prod Server"),
+                @Server(url = "http://localhost:8080", description = "localhost"),
+        }
+)
 @Configuration
 public class SwaggerConfig {
     private static final String BEARER_KEY = "bearer-key";
