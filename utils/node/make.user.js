1615103277bcd03c2da7b65479a402f594a5f3bf
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-08-22 18:50:22 +0900

    [Feat]: 유저 벌크 insert 추가(#56)

--- utils/node/make.user.js
+++
@@ -1,8 +0,0 @@
-import { fakerKO as faker } from "@faker-js/faker";
-for(let i = 0; i < 20; i++) {
-    const name = faker.person.firstName();
-    const imageUrl = faker.image.url();
-    const randomExp = Math.floor(Math.random() * 100);
-    const sql = `(${randomExp}, '2023-05-31 10:00:00', '2023-05-31 12:00:00', null, 'test${i}@a.c', '${name}', '$2a$10$h1VlKrjjHSnuRoeCnl1reOh.oaAw6EqxrSMB0FVClpOb1S2D.K.ZK', '${imageUrl}', null, 'USER'),`;
-    console.log(sql);
-}
