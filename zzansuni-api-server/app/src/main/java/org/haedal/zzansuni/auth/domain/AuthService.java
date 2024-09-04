9e60d0084c325888e3085e0fa1341bf6b269f397
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-04 13:48:17 +0900

    [Feat]: 리프래시토큰 재발급 API 추가 (#78)

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/auth/domain/AuthService.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/auth/domain/AuthService.java
@@ -1,10 +1,12 @@
 package org.haedal.zzansuni.auth.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.haedal.zzansuni.global.jwt.JwtToken;
 import org.haedal.zzansuni.global.jwt.JwtUser;
 import org.haedal.zzansuni.global.jwt.JwtUtils;
 import org.haedal.zzansuni.user.domain.*;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.util.Pair;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -12,17 +14,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class AuthService {
-
     private final List<OAuth2Client> oAuth2Clients;
     private final BCryptPasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
     private final UserReader userReader;
     private final UserStore userStore;
+    private final RefreshTokenStore refreshTokenStore;
+    private final RefreshTokenReader refreshTokenReader;
 
     /**
      * OAuth2 로그인 또는 회원가입 <br> [state]는 nullable한 입력 값이다.<br> 1. OAuth2Client를 이용해 해당 provider로부터
@@ -56,9 +62,29 @@ public class AuthService {
         return userStore.store(user);
     }
 
+    /**
+     * JWT 발급
+     * 1. 리프래시토큰의 uuid 생성
+     * 2. JWT 토큰 생성
+     * 3. DB에 리프래시토큰 정보를 저장, 리프래시토큰의 UUID가 PK이므로 중복이 발생하면 재시도 10회
+     */
     private JwtToken createToken(User user) {
         JwtUser jwtUser = JwtUser.of(user.getId(), user.getRole());
-        return jwtUtils.createToken(jwtUser);
+        //Retry 10 times
+        for (int i = 0; i < 10; i++) {
+            try {
+                String uuid = UUID.randomUUID().toString();
+
+                JwtToken jwtToken = jwtUtils.generateToken(jwtUser, uuid);
+                RefreshToken refreshToken = RefreshToken.create(uuid, user, jwtToken.getRefreshTokenExpireAt());
+                // Inner TX Scope
+                refreshTokenStore.store(refreshToken);
+                return jwtToken;
+            } catch (DataIntegrityViolationException e) {
+                log.debug("RefreshToken UUID 충돌. Retry: {}", i);
+            }
+        }
+        throw new RuntimeException("Failed to create token.");
     }
 
     @Transactional
@@ -84,7 +110,7 @@ public class AuthService {
         userStore.store(user);
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public Pair<JwtToken, UserModel.Main> login(String email, String password) {
         User user = userReader.findByEmail(email)
             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."));
@@ -98,13 +124,22 @@ public class AuthService {
         return Pair.of(jwtToken, userMain);
     }
 
-    public String reissueToken(String rawToken) {
-        if (!jwtUtils.validateToken(rawToken)) {
-            throw new IllegalArgumentException("RefreshToken이 유효하지 않습니다.");
-        }
-        JwtToken.ValidToken token = JwtToken.ValidToken.of(rawToken);
-        jwtUtils.reissueAccessToken(token);
+    @Transactional
+    public JwtToken reissueToken(String rawToken) {
+        JwtUtils.UserIdAndUuid userIdAndUuid = jwtUtils.validateAndGetUserIdAndUuid(rawToken);
 
-        return jwtUtils.reissueAccessToken(JwtToken.ValidToken.of(rawToken));
+        RefreshToken refreshToken = refreshTokenReader.findById(userIdAndUuid.uuid())
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 토큰입니다."));
+
+        // jwtUtils에서 이미 검증하였으나, 방어적으로 다시 한번 검증
+        if(!refreshToken.getUser().getId().equals(userIdAndUuid.userId())) {
+            throw new IllegalArgumentException("토큰의 유저정보가 일치하지 않습니다.");
+        }else if(refreshToken.getExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("만료된 토큰입니다.");
+        }
+
+        refreshTokenStore.delete(refreshToken.getId());
+        User user = refreshToken.getUser();
+        return createToken(user);
     }
 }
