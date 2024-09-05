56be2c73e249881ba3a4aacc2168fb1e40d2c3e8
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-05 18:06:10 +0900

    [Fix] Update V2__add_jwt.sql

--- zzansuni-api-server/app/src/main/resources/db/migration/V2__add_jwt.sql
+++ zzansuni-api-server/app/src/main/resources/db/migration/V2__add_jwt.sql
@@ -2,5 +2,5 @@ CREATE TABLE refresh_token (
     id CHAR(36) NOT NULL PRIMARY KEY,
     user_id BIGINT NOT NULL,
     expired_at TIMESTAMP(0) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
