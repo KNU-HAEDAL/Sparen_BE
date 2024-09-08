5e90a9ef08d50d08a0f5ecbbcdb91e2791ad219e
kwonssshyeon <kwonsh4396@gmail.com>
2024-09-08 18:53:52 +0900

    [Feat]: 챌린지 인증 조회 쿼리 작성 및 기능 구현

---
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/userchallenge/domain/application/ChallengeVerificationSerivce.java
@@ -0,0 +1,23 @@
+package org.haedal.zzansuni.userchallenge.domain.application;
+
+import lombok.RequiredArgsConstructor;
+import org.haedal.zzansuni.userchallenge.domain.ChallengeVerification;
+import org.haedal.zzansuni.userchallenge.domain.port.ChallengeVerificationReader;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ChallengeVerificationSerivce {
+    private final ChallengeVerificationReader challengeVerificationReader;
+
+
+    @Transactional(readOnly = true)
+    public Page<ChallengeVerificationModel.Admin> getChallengeVerifications(Pageable pageable,
+                                                                            String challengeTitle) {
+        Page<ChallengeVerification> verifications = challengeVerificationReader.getVerificationOrderByCreatedAt(pageable, challengeTitle);
+        return verifications.map(ChallengeVerificationModel.Admin::from);
+    }
+}
--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/userchallenge/infrastructure/adapter/ChallengeVerificationReaderImpl.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/userchallenge/infrastructure/adapter/ChallengeVerificationReaderImpl.java
@@ -3,17 +3,30 @@ package org.haedal.zzansuni.userchallenge.infrastructure.adapter;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.haedal.zzansuni.userchallenge.domain.ChallengeVerification;
 import org.haedal.zzansuni.userchallenge.domain.port.ChallengeVerificationReader;
 import org.haedal.zzansuni.userchallenge.infrastructure.ChallengeVerificationRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
+
+import static org.haedal.zzansuni.challengegroup.domain.QChallenge.challenge;
+import static org.haedal.zzansuni.challengegroup.domain.QChallengeGroup.challengeGroup;
+import static org.haedal.zzansuni.userchallenge.domain.QChallengeVerification.challengeVerification;
+import static org.haedal.zzansuni.userchallenge.domain.QUserChallenge.userChallenge;
 
 @Component
 @RequiredArgsConstructor
-public class ChallengeVerificationReaderImpl implements ChallengeVerificationReader {
+public class
+ChallengeVerificationReaderImpl implements ChallengeVerificationReader {
 
     private final ChallengeVerificationRepository challengeVerificationRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public ChallengeVerification getById(Long id) {
@@ -36,6 +49,36 @@ public class ChallengeVerificationReaderImpl implements ChallengeVerificationRea
     @Override
     public List<ChallengeVerification> findByUserChallengeId(Long id) {
         return challengeVerificationRepository.findByUserChallengeId(id);
+    }
+
+
+    @Override
+    public Page<ChallengeVerification> getVerificationOrderByCreatedAt(Pageable pageable, String challengeTitle) {
+        Long count = queryFactory
+                .select(challengeVerification.count())
+                .from(challengeVerification)
+                .innerJoin(challengeVerification.userChallenge, userChallenge)
+                .innerJoin(userChallenge.challenge, challenge)
+                .innerJoin(challenge.challengeGroup, challengeGroup)
+                .where(eqChallengeTitle(challengeTitle))
+                .fetchFirst();
+
+        List<ChallengeVerification> userChallenges = queryFactory
+                .selectFrom(challengeVerification)
+                .innerJoin(challengeVerification.userChallenge, userChallenge)
+                .innerJoin(userChallenge.challenge, challenge)
+                .innerJoin(challenge.challengeGroup, challengeGroup)
+                .where(eqChallengeTitle(challengeTitle))
+                .orderBy(challengeVerification.modifiedAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(userChallenges, pageable, count == null ? 0 : count);
+    }
+
+    private BooleanExpression eqChallengeTitle(String challengeTitle) {
+        return challengeTitle == null ? null : challengeGroup.title.contains(challengeTitle);
     }
 
 }
