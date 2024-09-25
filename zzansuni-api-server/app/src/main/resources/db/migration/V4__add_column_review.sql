7fd153da93dcc77f43aafa2bda07d35142cfa468
kwonssshyeon <104684033+kwonssshyeon@users.noreply.github.com>
2024-09-25 17:18:14 +0900

    Update zzansuni-api-server/app/src/main/resources/db/migration/V4__add_column_review.sql
    Co-authored-by: 손홍석 <78216059+bayy1216@users.noreply.github.com>

--- zzansuni-api-server/app/src/main/resources/db/migration/V4__add_column_review.sql
+++ zzansuni-api-server/app/src/main/resources/db/migration/V4__add_column_review.sql
@@ -1,2 +1,5 @@
-ALTER TABLE challenge_review ADD COLUMN difficulty BIGINT NOT NULL;
-ALTER TABLE challenge_review ADD COLUMN achievement BIGINT NOT NULL;
+ALTER TABLE challenge_review ADD COLUMN difficulty TINYINT DEFULT(1) NOT NULL;
+ALTER TABLE challenge_review ADD COLUMN achievement TINYINT DEFULT(1) NOT NULL;
+
+ALTER TABLE challenge_review ALTER COLUMN difficulty DROP DEFAULT;
+ALTER TABLE challenge_review ALTER COLUMN achievement DROP DEFAULT;
