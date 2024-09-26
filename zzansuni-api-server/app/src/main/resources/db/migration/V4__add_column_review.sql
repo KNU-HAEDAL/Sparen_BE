50b9db74704fade1eadadf9b680c557164175daf
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-26 14:15:57 +0900

    Update V4__add_column_review.sql

--- zzansuni-api-server/app/src/main/resources/db/migration/V4__add_column_review.sql
+++ zzansuni-api-server/app/src/main/resources/db/migration/V4__add_column_review.sql
@@ -1,5 +1,5 @@
-ALTER TABLE challenge_review ADD COLUMN difficulty TINYINT DEFULT(1) NOT NULL;
-ALTER TABLE challenge_review ADD COLUMN achievement TINYINT DEFULT(1) NOT NULL;
+ALTER TABLE challenge_review ADD COLUMN difficulty TINYINT DEFAULT(1) NOT NULL;
+ALTER TABLE challenge_review ADD COLUMN achievement TINYINT DEFAULT(1) NOT NULL;
 
 ALTER TABLE challenge_review ALTER COLUMN difficulty DROP DEFAULT;
 ALTER TABLE challenge_review ALTER COLUMN achievement DROP DEFAULT;
