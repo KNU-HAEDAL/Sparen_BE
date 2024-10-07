31db28462b0fdc71a9f520590a184788cc4b17e4
momnpa333 <momnpa333@gmail.com>
2024-10-07 16:10:05 +0900

    [Feat]: 내 랭킹 조회 api 제작

--- zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/user/controller/UserController.java
+++ zzansuni-api-server/app/src/main/java/org/haedal/zzansuni/user/controller/UserController.java
@@ -17,12 +17,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 public class UserController {
+
     private final UserService userService;
 
     @Operation(summary = "내 정보 조회", description = "내 정보를 조회한다.")
     @GetMapping("/api/user")
     public ApiResponse<UserRes.UserMyInfo> getUserInfo(
-            @AuthenticationPrincipal JwtUser jwtUser
+        @AuthenticationPrincipal JwtUser jwtUser
     ) {
         var userModel = userService.getUserModel(jwtUser.getId());
         return ApiResponse.success(UserRes.UserMyInfo.from(userModel));
@@ -31,8 +32,8 @@ public class UserController {
     @Operation(summary = "내 정보 수정", description = "내 정보를 수정한다.")
     @PutMapping("/api/user")
     public ApiResponse<Void> updateUser(
-            @Valid @RequestBody UserReq.Update request,
-            @AuthenticationPrincipal JwtUser jwtUser
+        @Valid @RequestBody UserReq.Update request,
+        @AuthenticationPrincipal JwtUser jwtUser
     ) {
         var command = request.toCommand();
         userService.updateUser(jwtUser.getId(), command);
@@ -42,16 +43,16 @@ public class UserController {
     @Operation(summary = "스트릭 조회", description = "스트릭을 조회한다.")
     @GetMapping("/api/user/streak")
     public ApiResponse<UserRes.Streak> getStreak(
-            @AuthenticationPrincipal JwtUser jwtUser,
-            UserReq.GetStreak request
+        @AuthenticationPrincipal JwtUser jwtUser,
+        UserReq.GetStreak request
     ) {
-        if(request.startDate().isAfter(request.endDate())){
+        if (request.startDate().isAfter(request.endDate())) {
             throw new IllegalArgumentException("시작일은 종료일보다 이전이어야 합니다.");
         }
         var userModelStreak = userService.getUserStreak(
-                jwtUser.getId(),
-                request.startDate(),
-                request.endDate()
+            jwtUser.getId(),
+            request.startDate(),
+            request.endDate()
         );
         return ApiResponse.success(UserRes.Streak.from(userModelStreak));
     }
@@ -59,10 +60,20 @@ public class UserController {
     @Operation(summary = "유저 랭킹 페이징", description = "전체 유저 랭킹을 조회 페이징")
     @GetMapping("/api/users/ranking")
     public ApiResponse<PagingResponse<UserRes.User>> getUsersRanking(
-            @Valid PagingRequest request
+        @Valid PagingRequest request
     ) {
         var userModelPage = userService.getUserPagingByRanking(request.toPageable());
         var response = PagingResponse.from(userModelPage, UserRes.User::from);
+        return ApiResponse.success(response);
+    }
+
+    @Operation(summary = "내 랭킹 조회", description = "유저 랭킹을 조회한다.")
+    @GetMapping("/api/users/my-ranking")
+    public ApiResponse<UserRes.MyRankingInfo> getMyRanking(
+        @AuthenticationPrincipal JwtUser jwtUser
+    ) {
+        var userModelRanking = userService.getMyRanking(jwtUser.getId());
+        var response = UserRes.MyRankingInfo.from(userModelRanking);
         return ApiResponse.success(response);
     }
 
